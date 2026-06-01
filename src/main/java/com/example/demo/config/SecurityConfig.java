7b5cc7afdbe239ec293c9fe0efff4059c4290c32
Simon <simonbetansosa@gmail.com>
2026-06-01 17:48:51 -0500

    arreglo endpoint

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
                 
                 // Todo lo demás privado
                 .anyRequest().authenticated()
+                .requestMatchers("/api/solicitudes/**").authenticated()
             )
             .oauth2Login(oauth2 -> oauth2
                 .successHandler(successHandler)
